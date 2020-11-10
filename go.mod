8d18d8789439d8bd9e1c1a49274b0089e9c4f8c9
Bill Chung <bill@mercari.com>
2020-11-10 21:36:02 +0900

    add cloud trace

--- go.mod
+++ go.mod
@@ -3,8 +3,10 @@ module github.com/billcchung/example-service
 go 1.14
 
 require (
-	cloud.google.com/go v0.71.0 // indirect
+	cloud.google.com/go v0.71.0
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.4
 	github.com/golang/protobuf v1.4.3
+	go.opencensus.io v0.22.5
 	google.golang.org/grpc v1.33.2
 	google.golang.org/protobuf v1.25.0
 )
--- ping/ping.go
+++ ping/ping.go
@@ -3,6 +3,7 @@ package Ping
 import (
 	"context"
 	pb "github.com/billcchung/example-service/proto"
+	"go.opencensus.io/trace"
 	"math/rand"
 	"time"
 )
@@ -12,6 +13,8 @@ var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
 type Server struct{}
 
 func (s Server) Get(ctx context.Context, req *pb.PingRequest) (res *pb.PingResponse, err error) {
+	ctx, span := trace.StartSpan(ctx, "Get")
+	defer span.End()
 	res = &pb.PingResponse{
 		Message_ID:  req.Message_ID,
 		MessageBody: req.MessageBody,
@@ -21,11 +24,15 @@ func (s Server) Get(ctx context.Context, req *pb.PingRequest) (res *pb.PingRespo
 }
 
 func (s Server) GetAfter(ctx context.Context, req *pb.PingRequestWithSleep) (res *pb.PingResponse, err error) {
+	ctx, span := trace.StartSpan(ctx, "GetAfter")
+	defer span.End()
 	time.Sleep(time.Duration(req.Sleep) * time.Second)
 	return s.Get(ctx, &pb.PingRequest{Message_ID: req.Message_ID, MessageBody: req.MessageBody})
 }
 
 func (s Server) GetRandom(ctx context.Context, req *pb.PingRequest) (res *pb.PingResponse, err error) {
+	ctx, span := trace.StartSpan(ctx, "GetRandom")
+	defer span.End()
 	var garbage []string
 	for i := 0; i <= 1000000; i++ {
 		garbage = append(garbage, string(letterRunes[rand.Intn(len(letterRunes))]))
